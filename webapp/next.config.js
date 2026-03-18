2c2aac207c7f8c030cb696cdac01f6ee7c2f9f81
tadschweizer <98231818+tadschweizer@users.noreply.github.com>
2026-03-18 12:07:14 -0700

    Add next.config.js

---
+++ webapp/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // Uncomment the following line if you deploy to a subpath
+  // basePath: '',
+};
+
+module.exports = nextConfig;
